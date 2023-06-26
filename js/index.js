35e586a11e3d270d8322b37d83b119ac3e390da8
Mohamed Ashraf <www.mohamedsalwa13@gmail.com>
2023-06-27 01:26:37 +0300

    Added the main files

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function calcHypo(a, b) {
+  return Math.sqrt(a * a + b * b);
+}
+
+document.getElementById("result").style.visibility = "hidden";
+
+document.getElementById("submit").onclick = () => {
+  let a = document.getElementById("a").value;
+  let b = document.getElementById("b").value;
+  document.getElementById("result").style.visibility = "visible";
+  document.getElementById("result").innerText = `النتيجة: ${calcHypo(a, b)}`;
+};
