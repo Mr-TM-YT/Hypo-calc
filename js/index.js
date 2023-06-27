f78778fb59ee3aa7089d81405302102da07350ed
Mohamed Ashraf <www.mohamedsalwa13@gmail.com>
2023-06-27 15:55:49 +0300

    Made the result text dynamic

--- js/index.js
+++ js/index.js
@@ -2,12 +2,12 @@ function calcHypo(a, b) {
   return Math.sqrt(a * a + b * b);
 }
 
-document.getElementById("result").style.visibility = "hidden";
+document.getElementById("result").style.display = "none";
 
 document.getElementById("submit").onclick = () => {
   let a = document.getElementById("a").value;
   let b = document.getElementById("b").value;
-  document.getElementById("result").style.visibility = "visible";
+  document.getElementById("result").style.display = "block";
   if (document.getElementById("precise").checked) {
     document.getElementById("result").innerText = `النتيجة : ${calcHypo(a, b)}`;
   } else {
