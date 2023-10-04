53c35e0c8c09d8762466d82b119c3d1cc7197f4f
Mr-TM-YT <www.mohamedsalwa13@gmail.com>
2023-10-04 21:42:50 +0300

    Used the built in hypot function

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
 function calcHypo(a, b) {
-  return Math.sqrt(a * a + b * b);
+	return Math.hypot(a, b);
 }
 
+// Hiding the text with the result
 document.getElementById("result").style.display = "none";
+
 
 document.getElementById("submit").onclick = () => {
   let a = document.getElementById("a").value;
   let b = document.getElementById("b").value;
   document.getElementById("result").style.display = "block";
+	// Checking if the user wants a very precise result
   if (document.getElementById("precise").checked) {
     document.getElementById("result").innerText = `النتيجة : ${calcHypo(a, b)}`;
   } else {
